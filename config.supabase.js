a435156e98e0698ce47336b337221e7a0107ada7
jleonardopainevilu-eng <jleonardo.painevilu@gmail.com>
2026-07-18 14:03:15 -0400

    Subir version 5 de mejoras comerciales

--- config.supabase.js
+++ config.supabase.js
@@ -1,2 +1,2 @@
 window.GATITO_SUPABASE={url:"",anonKey:""};
-(()=>{let s=document.createElement("script");s.src="/assets/comercial.js?v=4";s.defer=true;document.head.appendChild(s)})();
+(()=>{let s=document.createElement("script");s.src="/assets/comercial.js?v=5";s.defer=true;document.head.appendChild(s)})();
